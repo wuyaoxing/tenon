85ae6090a18c0fb38c34a4c23d8fcb2e261f07f8
wuyaoxing <82565097@qq.com>
2019-01-25 19:48:31 +0800

    chore: Add the template.

--- src/views/tenon/AddProject.vue
+++ src/views/tenon/AddProject.vue
@@ -34,77 +34,6 @@ export default {
                 1: {
                     name: 'NestedLayoutContainer',
                     properties: {
-                        minHeight: '100%'
-                    },
-                    children: [
-                        {
-                            name: 'Header',
-                            properties: {},
-                            children: []
-                        },
-                        {
-                            name: 'NestedLayoutContainer',
-                            properties: {},
-                            children: [
-                                {
-                                    name: 'NestedLayoutContainer',
-                                    properties: {},
-                                    children: [
-                                        {
-                                            name: 'NestedLayoutContainer',
-                                            properties: {},
-                                            children: []
-                                        },
-                                        {
-                                            name: 'PositionLayoutContainer',
-                                            properties: {
-                                            },
-                                            children: [
-                                                {
-                                                    name: 'Button',
-                                                    properties: {},
-                                                    children: []
-                                                },
-                                            ]
-                                        }
-                                    ]
-                                },
-                                {
-                                    name: 'NestedLayoutContainer',
-                                    properties: {},
-                                    children: []
-                                },
-                                {
-                                    name: 'PositionLayoutContainer',
-                                    properties: {
-                                    },
-                                    children: [
-                                        {
-                                            name: 'Button',
-                                            properties: {},
-                                            children: []
-                                        },
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            name: 'PositionLayoutContainer',
-                            properties: {
-                            },
-                            children: [
-                                {
-                                    name: 'Button',
-                                    properties: {},
-                                    children: []
-                                },
-                            ]
-                        }
-                    ],
-                },
-                2: {
-                    name: 'NestedLayoutContainer',
-                    properties: {
                         name: 'NestedLayoutContainer',
                         css: {
                             minHeight: '100%',
@@ -112,6 +41,97 @@ export default {
                         style: ''
                     },
                     children: []
+                },
+                2: {
+                    name: 'NestedLayoutContainer',
+                    properties: {
+                        name: 'NestedLayoutContainer',
+                        css: {
+                            minHeight: '', height: '100%', fontFamily: 'Raleway,sans-serif', fontSize: '1rem'
+                        },
+                        style: ''
+                    },
+                    children: [{
+                        name: 'NestedLayoutContainer',
+                        properties: {
+                            name: 'NestedLayoutContainer',
+                            css: {
+                                background: 'url(http://digibird.com.cn/areas/admin/static/article/201707/21/1540111742.png) no-repeat center bottom', height: '80%', backgroundSize: 'cover', minHeight: '', display: 'flex', alignItems: 'flex-end', justifyContent: 'center'
+                            },
+                            style: '',
+                            title: 'Tenon'
+                        },
+                        children: [{
+                            name: 'Header',
+                            properties: {
+                                name: 'Header',
+                                css: {
+                                    background: 'none', position: 'absolute', top: 0, right: 0, left: 0
+                                },
+                                style: '',
+                                title: 'Tenon'
+                            }
+                        }, {
+                            name: 'NestedLayoutContainer',
+                            properties: {
+                                name: 'NestedLayoutContainer',
+                                css: {
+                                    width: '', padding: '10px', textAlign: 'center', color: '#333333', fontWeight: '', lineHeight: '2', borderColor: null, margin: '0 0 80px'
+                                },
+                                style: '',
+                                title: 'Tenon'
+                            },
+                            children: [{
+                                name: 'Text',
+                                properties: {
+                                    name: 'Text', css: { fontSize: '6rem', fontWeight: '300', borderColor: null }, style: '', label: 'h1', text: '全可视化操作，所见即所得'
+                                }
+                            }, {
+                                name: 'Text',
+                                properties: {
+                                    name: 'Text', css: { fontWeight: '300', fontSize: '3rem' }, style: '', label: 'h2', text: '通过视频、音频、环境的可视化操作，让用户所见即所得，让系统更易用、更可靠。'
+                                }
+                            }]
+                        }]
+                    }, {
+                        name: 'NestedLayoutContainer',
+                        properties: {
+                            name: 'NestedLayoutContainer',
+                            css: {
+                                backgroundSize: 'cover', background: 'url(http://digibird.com.cn/images/dmis-6-1.jpg) no-repeat center bottom', height: '60%', backgroundColor: null, padding: '50px 0 0'
+                            },
+                            style: '',
+                            title: 'Tenon'
+                        },
+                        children: [{
+                            name: 'NestedLayoutContainer',
+                            properties: {
+                                name: 'NestedLayoutContainer',
+                                css: {
+                                    textAlign: 'center', padding: '10px', marginTop: 0, margin: '', lineHeight: '2'
+                                },
+                                style: '',
+                                title: 'Tenon'
+                            },
+                            children: [{
+                                name: 'Text',
+                                properties: {
+                                    name: 'Text', css: { fontSize: '4rem', fontWeight: '300' }, style: '', label: 'h2', text: '模块化设计，按需灵活配置'
+                                }
+                            }, {
+                                name: 'Text',
+                                properties: {
+                                    name: 'Text', css: { fontSize: '2rem', fontWeight: '300' }, style: '', label: 'h3', text: 'DMIS系统包含管理平台、视频处理模块、音频处理模块、环境控制模块以及操作交互终端，可随需灵活配置。'
+                                }
+                            }]
+                        }]
+                    }, {
+                        name: 'NestedLayoutContainer',
+                        properties: {
+                            name: 'NestedLayoutContainer', css: { height: '100%' }, style: '', title: 'Tenon'
+                        },
+                        children: []
+                    }]
                 }
             }
         }
@@ -148,8 +168,8 @@ export default {
                 if (item.children) item.children = recursion(item.children)
                 console.log(item)
                 return {
-                    id: this.$uuid(),
-                    ...item
+                    ...item,
+                    id: this.$uuid()
                 }
             })
 
